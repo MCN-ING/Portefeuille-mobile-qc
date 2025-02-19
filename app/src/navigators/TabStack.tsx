f30b4803017beac78504753a6038ccdec5956de4
ClaudeArs <107423901+ClaudeArs@users.noreply.github.com>
2025-02-19 13:06:04 -0500

    Fermer l'aide qand le status est inactif (#270)
    Signed-off-by: ClaudeArs <claudears2474@gmail.com>

--- app/src/navigators/TabStack.tsx
+++ app/src/navigators/TabStack.tsx
@@ -10,6 +10,7 @@ import {
   TOKENS,
   connectFromScanOrDeepLink,
   testIdWithKey,
+  useActivity,
   useServices,
   useStore,
   useTheme,
@@ -70,6 +71,12 @@ const TabStack: React.FC = () => {
   const Tab = createBottomTabNavigator<TabStackParams>()
   const { ColorPallet, TabTheme, TextTheme } = useTheme()
   const showLabels = fontScale * TabTheme.tabBarTextStyle.fontSize < 18
+
+  const { appStateStatus } = useActivity()
+
+  useEffect(() => {
+    if (appStateStatus !== 'active') DeviceEventEmitter.emit(BCWalletEventTypes.ADD_HELP_PRESSED, { isActive: false })
+  }, [appStateStatus])
 
   const logHistoryRecord = useCallback(
     async (credential: CredentialRecord) => {
