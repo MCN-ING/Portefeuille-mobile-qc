06e7d90fbd5fc1b5126dc0599ac306e4818f1523
ClaudeArs <claudears2474@gmail.com>
2025-02-28 14:08:50 -0500

    Empecher TalkBack de lire sous le Modal
    Signed-off-by: ClaudeArs <claudears2474@gmail.com>

--- app/src/components/modals/CustomModal.tsx
+++ app/src/components/modals/CustomModal.tsx
@@ -78,7 +78,7 @@ export const CustomModal = ({ title, description, primary, secondary, onDismissP
     <Modal transparent accessibilityViewIsModal testID={testIdWithKey('CustomModal')}>
       <TouchableOpacity onPress={onDismissPressed} accessible={false}>
         <View style={styles.modalCenter}>
-          <TouchableWithoutFeedback accessible={false}>
+          <TouchableWithoutFeedback accessible={true} accessibilityLabel="Modal">
             <View style={styles.container}>
               <View style={styles.titleContainer}>
                 <HeaderText title={title} />
